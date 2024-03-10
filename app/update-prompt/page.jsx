0bf96153e3e81c6e1644f1620c9984d67755966f
Nguyen Duong <nduongg04@gmail.com>
2024-03-10 23:57:48 +0700

    Remove console.log statement

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect, useDebugValue } from "react";
+import { useState, useEffect } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 
 import Form from "@components/Form";
