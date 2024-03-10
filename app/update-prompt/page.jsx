285f068cea85aaa682b7b3fdea34820a15a36f89
Nguyen Duong <nduongg04@gmail.com>
2024-03-11 00:15:01 +0700

    Fix useRouter from next/router

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,7 +1,8 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { useRouter, useSearchParams } from "next/navigation";
+import { useSearchParams } from "next/navigation";
+import { useRouter } from "next/router";
 
 import Form from "@components/Form";
 
