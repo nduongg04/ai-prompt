9f430ad1612fbeca097cc7a6bb97d6310296e75f
Nguyen Duong <nduongg04@gmail.com>
2024-03-10 23:35:13 +0700

    Ignore .env file and fix formatting in database.js

--- utils/database.js
+++ utils/database.js
@@ -3,23 +3,21 @@ import mongoose from "mongoose";
 let isConnected = false; // track the connection status
 
 export const connectToDB = async () => {
-    mongoose.set('strictQuery', true);
+	mongoose.set("strictQuery", true);
 
-    if (isConnected) {
-        console.log("MongoDB is already connected");
-        return;
-    }
+	if (isConnected) {
+		console.log("MongoDB is already connected");
+		return;
+	}
 
-    try {
-			await mongoose.connect(process.env.MONGODB_URI, {
-				dbName: "share_prompt",
-				useNewUrlParser: true,
-				useUnifiedTopology: true,
-			});
+	try {
+		await mongoose.connect(process.env.MONGODB_URI, {
+			dbName: "share_prompt",
+		});
 
-			isConnected = true;
-			console.log("MongoDB connected");
-		} catch (error) {
-			console.log(error);
-		}
-}
+		isConnected = true;
+		console.log("MongoDB connected");
+	} catch (error) {
+		console.log(error);
+	}
+};
